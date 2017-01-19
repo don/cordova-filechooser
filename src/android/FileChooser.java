909a7ee7ceb06e858d2073733b27f8120c3a816e
Dilhan Jayathilake <dilhan.jayathilaka@live.com>
2017-01-19 15:26:39 +1100

    added support for non local file selection
    This will add support for selecting files from Google Drive

--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -1,9 +1,14 @@
 package com.megster.cordova;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
+import java.io.File;
+import java.io.IOException;
+import android.database.Cursor;
+import android.provider.OpenableColumns;
 
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CallbackContext;
@@ -36,7 +41,8 @@ public class FileChooser extends CordovaPlugin {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("*/*");
         intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+
+        //intent.putExtra(Intent.EXTRA_LOCAL_ONLY, false);
 
         Intent chooser = Intent.createChooser(intent, "Select File");
         cordova.startActivityForResult(this, chooser, PICK_FILE_REQUEST);
@@ -59,24 +65,76 @@ public class FileChooser extends CordovaPlugin {
                 if (uri != null) {
 
                     Log.w(TAG, uri.toString());
-                    callback.success(uri.toString());
 
-                } else {
+                    try {
+                        Context context = cordova.getActivity();
+                        String mimeType = context.getContentResolver().getType(uri);
 
-                    callback.error("File uri was null");
+                        Uri returnUri = data.getData();
+                        Cursor returnCursor = context.getContentResolver().query(returnUri, null, null, null, null);
+                        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                        int sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE);
+                        returnCursor.moveToFirst();
+                        String filename = returnCursor.getString(nameIndex);
 
+                        File fileSave = context.getExternalFilesDir(null);
+                        String sourcePath = context.getExternalFilesDir(null).toString();
+
+                        try {
+
+                            File savedFile = new File(sourcePath + "/" + filename);
+
+                            copyFileStream(savedFile, uri, context);
+
+                            callback.success(savedFile.toURI().toString());
+
+                        } catch (Exception e) {
+                            Log.w(TAG, e.getMessage());
+                        }
+
+                    } catch (Exception e) {
+                        Log.w(TAG, e.getMessage());
+                    }
                 }
 
-            } else if (resultCode == Activity.RESULT_CANCELED) {
-
-                // TODO NO_RESULT or error callback?
-                PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
-                callback.sendPluginResult(pluginResult);
 
             } else {
 
-                callback.error(resultCode);
+                callback.error("File uri was null");
+
             }
+
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+
+            // TODO NO_RESULT or error callback?
+            PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+            callback.sendPluginResult(pluginResult);
+
+        } else {
+
+            callback.error(resultCode);
         }
     }
+
+    private void copyFileStream(File dest, Uri uri, Context context) throws IOException {
+        java.io.InputStream is = null;
+        java.io.OutputStream os = null;
+        try {
+            is = context.getContentResolver().openInputStream(uri);
+            os = new java.io.FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            is.close();
+            os.close();
+        }
+    }
+
 }
